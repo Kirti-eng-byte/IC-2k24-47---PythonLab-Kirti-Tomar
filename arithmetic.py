445c0161d0024b536428aae63e06d6d4e3742b49
Kirti-eng-byte <kirtitomar185@gmail.com>
2026-09-08 23:20:57 +0530

    Delete arithmetic.py

--- arithmetic.py
+++
@@ -1,8 +0,0 @@
-# Arithmetic Operations
-a = float(input("Enter first number: "))
-b = float(input("Enter second number: "))
-print(f"Sum: {a+b}")
-print(f"Difference: {a-b}")
-print(f"Product: {a*b}")
-print(f"Quotient: {a/b}")
-print(f"Remainder: {a%b}")

a03796d89ea16ed031251ba55e37794ce744b3f9
Kirti-eng-byte <kirtitomar185@gmail.com>
2026-09-08 23:22:12 +0530

    Delete variable_practice.py

--- variable_practice.py
+++
@@ -1,9 +0,0 @@
-# Variable and Identifier Practice
-my_name = "Aman"
-age = 20
-height = 5.9
-is_student = True
-print(my_name, type(my_name))
-print(age, type(age))
-print(height, type(height))
-print(is_student, type(is_student))
